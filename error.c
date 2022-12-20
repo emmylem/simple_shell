6d2bb746c80a476132f962c63902dd9f8a90ae77
emmylemk9 <benjaminosondu55@gmail.com>
2022-12-20 14:23:42 -0800

    error.c Update to the error builtin command

--- error.c
+++ error.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 
 int create_error(char **args, int err);
+int len_num(int num);
+char *_atos(int num);
 
 /**
  * create_error - writes a error message to stderr
@@ -42,4 +44,73 @@ int create_error(char **args, int err)
 	if (error)
 		free(error);
 	return (err);
+}
+
+/**
+ * len_num - counts lengths of digit
+ * @num: number to measure
+ *
+ * Return: digit length
+ */
+int len_num(int num)
+{
+	unsigned int Num1;
+	int len = 1;
+
+	if (num < 0)
+	{
+		len++;
+		Num1 = num * -1;
+	}
+	else
+	{
+		Num1 = num;
+	}
+	while (Num1 > 9)
+	{
+		len++;
+		Num1 /= 10;
+	}
+
+	return (len);
+}
+
+/**
+ * _atos - opposite of atoi func but this
+ *           to convert integer to string
+ * @num: the integer
+ * Return: converted string
+ */
+char *_atos(int num)
+{
+	char *buffer;
+	int len = len_num(num);
+	unsigned int Num1;
+
+	buffer = malloc(sizeof(char) * (len + 1));
+	if (!buffer)
+		return (NULL);
+
+	buffer[len] = '\0';
+
+	if (num < 0)
+	{
+		Num1 = num * -1;
+		buffer[0] = '-';
+	}
+	else
+	{
+		Num1 = num;
+	}
+
+	len--;
+
+	while (Num1 > 0)
+	{
+		buffer[len] = (Num1 % 10) + '0';
+		Num1 = Num1 / 10;
+		len--;
+	}
+
+	return (buffer);
 }
