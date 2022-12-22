e4781ccafde83568ea144165160a982e9661991c
emmylemk9 <benjaminosondu55@gmail.com>
2022-12-21 17:05:33 -0800

    main.c

--- main.c
+++ main.c
@@ -1,21 +1,71 @@
 #include "shell.h"
-void shell(int ac, char **av, char **env);
+void sig_handler(int sig);
+int execute(char **args, char **front);
+/**
+ * sig_handler - prints
+ * @sig: the signal
+ */
+void sig_handler(int sig)
+{
+	char *new_prompt = "\n$ ";
+
+	(void)sig;
+	signal(SIGINT, sig_handler);
+	write(STDIN_FILENO, new_prompt, 3);
+}
 
 /**
- * main - entyr point
- * @ac: arg count
- * @av: array
- * @env: environment
- * Return: 0;
+ * execute - executes
+ * @args: array
+ * @front: poimter
+ * Return: exit value
  */
-int main(int ac, char **av, char **env)
+int execute(char **args, char **front)
 {
-	if (!ac)
-		(void)ac;
-	if (!av)
-		(void)av;
-	if (!env)
-		(void)env;
-	shell(ac, av, env);
-	return (0);
+	pid_t child_pid;
+	int status, flag = 0, ret = 0;
+	char *command = args[0];
+
+	if (command[0] != '/' && command[0] != '.')
+	{
+		flag = 1;
+		command = get_location(command);
+	}
+	if (!command || (access(command, F_OK) == -1))
+	{
+		if (errno == EACCES)
+			ret = (create_error(args, 126));
+		else
+			ret = (create_error(args, 127));
+	}
+	else
+	{
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			if (flag)
+				free(command);
+			perror("ERROR child:");
+			return (1);
+		}
+		if (child_pid == 0)
+		{
+			execve(command, args, environ);
+			if (errno == EACCES)
+				ret = (create_error(args, 126));
+			free_env();
+			free_args(args, front);
+
+			free_alias_list(aliases);
+			_exit(ret);
+		}
+		else
+		{
+			wait(&status);
+			ret = WEXITSTATUS(status);
+		}
+	}
+	if (flag)
+		free(command);
+	return (ret);
 }
